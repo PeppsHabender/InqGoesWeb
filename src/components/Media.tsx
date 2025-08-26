d3e9fa5d322968e24d097917fd7e0c8e95fd2c84
PeppsHabender <mail@peppshabender.de>
2025-08-26 14:27:05 +0200

    Update media

--- src/components/Media.tsx
+++ src/components/Media.tsx
@@ -1,9 +1,22 @@
 import {useEffect, useState} from "react";
 import Typography from "./Typography.tsx";
 
+function shuffle<T>(array: T[]): T[] {
+    let currentIndex = array.length;
+
+    while (currentIndex !== 0) {
+        const randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+    }
+
+    return array;
+}
+
+
 const Media = () => {
     const [images, setImages] = useState<string[]>([]);
-    const [selected, setSelected] = useState(0);
 
     useEffect(() => {
         const imageModules = import.meta.glob('/src/assets/media/*.{png,jpg,jpeg,svg}', {
@@ -11,21 +24,14 @@ const Media = () => {
             as: 'url',
         });
 
-        setImages(Object.values(imageModules));
+        setImages(shuffle(Object.values(imageModules)));
     }, []);
 
     return (
-        <div className="flex flex-col space-y-5 p-5 md:p10 h-full overflow-hidden content-center justify-center">
+        <div className="flex flex-col space-y-5 p-5 sm:max-h-screen sm:overflow-scroll md:max-h-none md:overflow-hidden md:p10 h-full content-center justify-center">
             <Typography variant="h2">Intercepted Media</Typography>
-            <div>
-                <img className="m-auto h-auto max-w-full rounded-lg object-cover object-center" src={images[selected]} alt=""/>
-            </div>
-            <div className="flex flex-wrap w-full space-y-5 space-x-5 overflow-scroll justify-center p-2">
-                {
-                    images.map((image, idx) => <div onClick={() => setSelected(idx)}>
-                        <img className={`h-auto w-20 md:w-40 lg:w-50 xl:w-60 rounded-lg ${selected == idx ? "scale-[105%] ring-2 ring-dark-red" : "cursor-pointer hover:scale-[105%]"}`} src={image} alt=""/>
-                    </div>)
-                }
+            <div className="m-10 columns-1 sm:columns-2 md:columns-3 lg:columns-4 gap-10 space-y-10">
+                {images.map((image) => <img className="rounded-md" src={image} alt=""/>)}
             </div>
         </div>
     )
